df097b2a59108759e65ecae81f49f8c692f9f38f
N4THB3N <ncastaneda@basiscons.com>
2022-10-10 18:48:39 -0600

    Adding CSS

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,8 +1,16 @@
 let ataqueJugador
 let ataqueEnemigo
+let vidasJugador = 3
+let vidasEnemigo = 3
 
 function iniciarJuego()
 {
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'none'
+
+    let sectionReiniciar = document.getElementById('reiniciar')
+    sectionReiniciar.style.display = 'none'
+
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 
@@ -12,6 +20,13 @@ function iniciarJuego()
     botonAgua.addEventListener('click', ataqueAgua)
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
+
+    let botonReiniciar = document.getElementById("boton-reiniciar")
+    botonReiniciar.addEventListener('click', reiniciarJuego)
+}
+
+function reiniciarJuego(){
+    location.reload()
 }
 
 function aleatorio(min, max)
@@ -21,6 +36,13 @@ function aleatorio(min, max)
 
 function seleccionarMascotaJugador()
 {
+
+    let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
+    sectionSeleccionarMascota.style.display = 'none'
+
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'block'
+
     let inputHipodogue = document.getElementById('hipodogue')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
@@ -57,19 +79,19 @@ function seleccionarMascotaEnemigo()
 
 function ataqueFuego()
 {
-    ataqueJugador = 'Fuego'
+    ataqueJugador = 'FUEGO'
     ataqueAleatorioEnemigo()
 }
 
 function ataqueAgua()
 {
-    ataqueJugador = 'Agua'
+    ataqueJugador = 'AGUA'
     ataqueAleatorioEnemigo()
 }
 
 function ataqueTierra()
 {
-    ataqueJugador = 'Tierra'
+    ataqueJugador = 'TIERRA'
     ataqueAleatorioEnemigo()
 }
 
@@ -77,13 +99,71 @@ function ataqueAleatorioEnemigo()
 {
     let ataqueeAleatorio = aleatorio(1,3)
     if(ataqueEnemigo == 1){
-        ataqueEnemigo = 'Fuego'
+        ataqueEnemigo = 'FUEGO'
     }else if (ataqueeAleatorio == 2){
-        ataqueEnemigo = 'Agua'
+        ataqueEnemigo = 'AGUA'
     }else{
-        ataqueEnemigo = 'Tierra'
+        ataqueEnemigo = 'TIERRA'
+    }
+    combate()
+}
+
+function combate(){
+
+    let spanVidasJugador = document.getElementById("vidas-jugador")
+    let spanVidasEnemigo = document.getElementById("vidas-enemigo")
+
+    if(ataqueEnemigo==ataqueJugador){
+        crearMensaje("EMPATE")
+    }else if((ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA') || (ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO') || (ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA')){
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    }else{
+        crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
+    }
+    
+    revisarVidas()
+}
+
+function revisarVidas(){
+
+    let sectionReiniciar = document.getElementById('reiniciar')   
+    
+    if(vidasEnemigo == 0){
+        crearMensajeFinal("FELICITACIONES. GANASTE")        
+        sectionReiniciar.style.display = 'block'
+    }else if(vidasJugador == 0){
+        crearMensajeFinal("LO SENTIMOS. PERDISTE")
+        sectionReiniciar.style.display = 'block'
     }
 }
+
+function crearMensaje(resultado)
+{
+    let sectionMensajes = document.getElementById('mensajes')
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ', la mascota del enemigo atacó con ' + ataqueEnemigo + ' ' + resultado
+    sectionMensajes.appendChild(parrafo)
+}
+
+function crearMensajeFinal(resultadoFinal)
+{
+    let sectionMensajes = document.getElementById('mensajes')
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = resultadoFinal
+    sectionMensajes.appendChild(parrafo)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled = true    
+}
+
 
 function aleatorio(min, max)
 {
